e46d67e9994a849b0d58128a3ce0d6436d5acd11
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-17 15:08:31 +0200

    update module 7-1

---
+++ js/module-7-1.js
@@ -0,0 +1,21 @@
+{
+    /** Приклад з конспекту 7/1 Розділ 2 */
+    // const parent = document.querySelector("#parent");
+    // const child = document.querySelector("#child");
+    // const descendant = document.querySelector("#descendant");
+
+    // parent.addEventListener("click", () => {
+    //     alert("Parent click handler");
+    //     console.log("Parent click handler");
+    // });
+
+    // child.addEventListener("click", () => {
+    //     alert("Child click handler");
+    //     console.log("Child click handler");
+    // });
+
+    // descendant.addEventListener("click", () => {
+    //     alert("Descendant click handler");
+    //     console.log("Descendant click handler");
+    // });   
+}
