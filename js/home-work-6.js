4d95d5fb3a9f799c509a58d6e4f810cc520cdc22
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-17 14:33:59 +0200

    create home-work-6

---
+++ js/home-work-6.js
@@ -0,0 +1,12 @@
+/** Задача 3 */
+{
+    // var counterValue = 0;
+    // function increment() {
+    //     counterValue += 1;
+    //     document.getElementById('value').innerHTML = counterValue;
+    // }
+    // function decrement(){
+    //     counterValue -= 1;
+    //     document.getElementById('value').innerHTML = counterValue;
+    // }
+}
