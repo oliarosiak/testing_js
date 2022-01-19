6235ffeb9713d296cfada4911bc61dd38848e5c6
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-19 17:40:44 +0200

    Update module-7-1.js

--- js/module-7-1.js
+++ js/module-7-1.js
@@ -1,22 +1,28 @@
 {
     /** Приклад з конспекту 7/1 Розділ 2 */
     const parent = document.querySelector("#parent");
-    // const child = document.querySelector("#child");
-    // const descendant = document.querySelector("#descendant");
+    const child = document.querySelector("#child");
+    const descendant = document.querySelector("#descendant");
 
-    // parent.addEventListener("click", () => {
-    //     alert("Parent click handler");
-    //     console.log("Parent click handler");
+    parent.addEventListener("click", (event) => {
+        console.log("event.target: ", event.target);
+        console.log("event.currentTarget: ", event.currentTarget);
+        //alert("Parent click handler");
+        //console.log("Parent click handler");
+    });
+
+    // child.addEventListener("click", (event) => {
+    //     console.log("event.target: ", event.target);
+    //     console.log("event.currentTarget: ", event.currentTarget);
+    //     //alert("Child click handler");
+    //     //console.log("Child click handler");
     // });
 
-    // child.addEventListener("click", () => {
-    //     alert("Child click handler");
-    //     console.log("Child click handler");
-    // });
-
-    // descendant.addEventListener("click", () => {
-    //     alert("Descendant click handler");
-    //     console.log("Descendant click handler");
+    // descendant.addEventListener("click", (event) => {
+    //     console.log("event.target: ", event.target);
+    //     console.log("event.currentTarget: ", event.currentTarget);
+    //     //alert("Descendant click handler");
+    //     //console.log("Descendant click handler");
     // });   
 
     /** */
@@ -47,46 +53,46 @@
 }
 
 {
-    const colorPalette = document.querySelector(".color-palette");
-    const output = document.querySelector(".output");
+    // const colorPalette = document.querySelector(".color-palette");
+    // const output = document.querySelector(".output");
 
-    colorPalette.addEventListener("click", selectColor);
+    // colorPalette.addEventListener("click", selectColor);
 
-    // This is where delegation «magic» happens
-    function selectColor(event) {
-        if (event.target.nodeName !== "BUTTON") {
-            return;
-        }
+    // // This is where delegation «magic» happens
+    // function selectColor(event) {
+    //     if (event.target.nodeName !== "BUTTON") {
+    //         return;
+    //     }
 
-        const selectedColor = event.target.dataset.color;
-        output.textContent = `Selected color: ${selectedColor}`;
-        output.style.color = selectedColor;
-    }
+    //     const selectedColor = event.target.dataset.color;
+    //     output.textContent = `Selected color: ${selectedColor}`;
+    //     output.style.color = selectedColor;
+    // }
 
-    // Some helper functions to render palette items
-    createPaletteItems();
+    // // Some helper functions to render palette items
+    // createPaletteItems();
 
-    function createPaletteItems() {
-        const items = [];
-        for (let i = 0; i < 60; i++) {
-            const color = getRangomColor();
-            const item = document.createElement("button");
-            item.type = "button";
-            item.dataset.color = color;
-            item.style.backgroundColor = color;
-            item.classList.add("item");
-            items.push(item);
-        }
-        colorPalette.append(...items);
-    }
+    // function createPaletteItems() {
+    //     const items = [];
+    //     for (let i = 0; i < 60; i++) {
+    //         const color = getRangomColor();
+    //         const item = document.createElement("button");
+    //         item.type = "button";
+    //         item.dataset.color = color;
+    //         item.style.backgroundColor = color;
+    //         item.classList.add("item");
+    //         items.push(item);
+    //     }
+    //     colorPalette.append(...items);
+    // }
 
-    function getRangomColor() {
-        return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
-    }
+    // function getRangomColor() {
+    //     return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+    // }
 
-    function getRandomHex() {
-        return Math.round(Math.random() * 256)
-            .toString(16)
-            .padStart(2, "0");
-    }
+    // function getRandomHex() {
+    //     return Math.round(Math.random() * 256)
+    //         .toString(16)
+    //         .padStart(2, "0");
+    // }
 }
