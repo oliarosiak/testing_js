fedd9e1cbab01cb541928009682e3054e76feef7
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-22 23:56:49 +0200

    Update bot-1.js

--- js/bot-1.js
+++ js/bot-1.js
@@ -4,7 +4,6 @@
     const pricePerItem = 2000;
     console.log('Задача 1:', 'Строка:', productName, 'Число:',pricePerItem); // 'Droid' // 2000
 }
-
 /** Задача 1-2 */
 {
     let productName = "Droid";
@@ -15,7 +14,6 @@
 
     console.log('Задача 2:', 'name:', productName, 'price:', pricePerItem);
 }
-
 /** Задача 1-3 */
 {
     const topSpeed = 160;
@@ -26,11 +24,37 @@
 
     console.log('Задача 3:', 'topSpeed', topSpeed, 'distance', distance, 'login', login, 'isOnline', isOnline, 'isAdmin', isAdmin);
 }
-
 /** Задача 1-4 */
+{
+    const pricePerItem = 3500;
+    const orderedQuantity = 4;
+
+    const totalPrice = pricePerItem * orderedQuantity;
+    console.log('Задача 4:', 'totalPrice:', totalPrice);
+}
 /** Задача 1-5 */
+{
+    const productName = "Droid";
+    const pricePerItem = 3500;
+
+    const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+    console.log('Задача 5:', 'message:', message);
+}
 /** Задача 1-6 */
+{
+    const pricePerDroid = 800;
+    const orderedQuantity = 6;
+    const deliveryFee = 50;
+    const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+    const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+    console.log('Задача 6:', 'message:', message);
+}
 /** Задача 1-7 */
+{
+    
+}
 /** Задача 1-8 */
 /** Задача 1-9 */
 /** Задача 1-10 */
