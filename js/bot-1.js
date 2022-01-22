786191a5812cd767ce084bab3b76e1783d47f387
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-22 22:44:42 +0200

    update

---
+++ js/bot-1.js
@@ -0,0 +1,37 @@
+/** Задача 1-1 */
+{
+    const productName = 'Droid';
+    const pricePerItem = 2000;
+    console.log('Задача 1:', 'Строка:', productName, 'Число:',pricePerItem); // 'Droid' // 2000
+}
+
+/** Задача 1-2 */
+{
+    let productName = "Droid";
+    let pricePerItem = 2000;
+
+    productName = 'Repair droid';
+    pricePerItem = 1500; 
+
+    console.log('Задача 2:', 'name:', productName, 'price:', pricePerItem);
+}
+
+/** Задача 1-3 */
+{
+    const topSpeed = 160;
+    const distance = 617.54;
+    const login = 'mango935';
+    const isOnline = true;
+    const isAdmin = false;
+
+    console.log('Задача 3:', 'topSpeed', topSpeed, 'distance', distance, 'login', login, 'isOnline', isOnline, 'isAdmin', isAdmin);
+}
+
+/** Задача 1-4 */
+/** Задача 1-5 */
+/** Задача 1-6 */
+/** Задача 1-7 */
+/** Задача 1-8 */
+/** Задача 1-9 */
+/** Задача 1-10 */
+
