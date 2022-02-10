239c757a8445bafe96e870dbeace74bf8c6d5c80
Olia Rosiak <olia.rosiak@gmail.com>
2022-02-10 18:57:20 +0200

    add pagination

---
+++ js/pagination.js
@@ -0,0 +1,34 @@
+const fetchPostsBtn = document.querySelector(".btn");
+const userList = document.querySelector(".posts");
+
+fetchPostsBtn.addEventListener("click", () => {
+  fetchPosts()
+    .then((posts) => renderPosts(posts))
+    .catch((error) => console.log(error));
+});
+
+function fetchPosts() {
+  // Change the number of items in the group here  
+  return fetch("https://jsonplaceholder.typicode.com/posts?_limit=15").then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
+
+function renderPosts(posts) {
+  const markup = posts
+    .map(({ id, title, body, userId }) => {
+      return `<li>
+          <h2 class="post-title">${title.slice(0, 30)}</h2>
+          <p><b>Post id</b>: ${id}</p>
+          <p><b>Author id</b>: ${userId}</p>
+          <p class="post-body">${body}</p>
+        </li>`;
+    })
+    .join("");
+  userList.innerHTML = markup;
+}
